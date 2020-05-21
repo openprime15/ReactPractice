548e3148b820b979c4f39e096d9ab26cce08c72f
openprime15 <openprime15@gmail.com>
2020-05-22 00:04:14 +0900

    acorn modify

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Greetings from "./Components/Greetings";
 
 function App() {
-  return <Greetings name="World" />;
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`);
+  };
+
+  return <Greetings name="React" onClick={onClick} />;
 }
 
 export default App;
