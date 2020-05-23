c1277837ac5a9779a9ce31ce102982f6bcffc109
openprime15 <openprime15@gmail.com>
2020-05-23 23:01:38 +0900

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Greetings from "./Components/Greetings";
+import Counter from "./Components/Counter";
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
+  // const onClick = (name: string) => {
+  //   console.log(`${name} says hello`);
+  // };
 
-  return <Greetings name="React" onClick={onClick} />;
+  // return <Greetings name="React" onClick={onClick} />;
+  return <Counter />;
 }
 
 export default App;
